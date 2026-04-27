df6fac54d6b7dbfea915bb4c8379ade72f0d26c3
FrontEndMaestro <abdulahadabbassi2@gmail.com>
2026-04-27 20:09:13 +0500

    feat: pass click handler and state props to Card
    Wire up individual Card components to support the parent application's
    interaction and score tracking logic.
    
    - Accept `id` and `clicked` boolean as props for card identification
    - Accept a click callback function to trigger state updates in pokemonData.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,9 @@
 import "../styles/card.css";
 
-export default function Card({ name, imgsrc }) {
+export default function Card({ id, name, imgsrc, clicked, cardClick }) {
     
   return (
-    <div className="card">
+    <div className="card" onClick={() => cardClick(id, clicked)}>
       <img src={imgsrc} alt="pokemon" id="pokemon-image" />
       <label htmlFor="pokemon-image">{name.toUpperCase()}</label>
     </div>
--- src/components/CardsSection.jsx
+++ src/components/CardsSection.jsx
@@ -1,14 +1,16 @@
 import Card from "./Card.jsx";
-import '../styles/cardSection.css'
-export default function CardsSection({ pokemonDataArray }) {
+import "../styles/cardSection.css";
+export default function CardsSection({ pokemonDataArray, cardClick }) {
   return (
     <section className="card-container">
       {pokemonDataArray.map((pokemon) => (
         <Card
           key={pokemon.id}
+          id={pokemon.id}
           name={pokemon.name}
           imgsrc={pokemon.image}
-          
+          clicked={pokemon.clicked}
+          cardClick={cardClick}
         ></Card>
       ))}
     </section>
